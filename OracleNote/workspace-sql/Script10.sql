7b77a9c9995819fd676e125836b8d4c33aaa789a
jaemikim <true0208@naver.com>
2023-11-03 09:19:33 +0900

    20231102

--- OracleNote/workspace-sql/Script10.sql
+++ OracleNote/workspace-sql/Script10.sql
@@ -73,3 +73,36 @@ FROM TB_EMP A JOIN TB_DEPT B ON (A.DEPT_CD = B.DEPT_CD)
 WHERE A.ADDR LIKE '%수원%'
 ;
 
+
+-- 주소가 수원인 직원의 사원번호, 사원명, 주소, 부서코드, 부서명, 자격증 코드를 출력하시오.
+-- 3개의 테이블을 조인하는데 조인 조건은 최소 2개가 있어야 한다.
+SELECT 
+	A.EMP_NO 
+	, A.EMP_NM 
+	, A.ADDR 
+	, B.DEPT_CD 
+	, B.DEPT_NM 
+	, C.CERTI_CD 
+FROM TB_EMP A
+	, TB_DEPT B
+	, TB_EMP_CERTI C
+WHERE A.DEPT_CD = B.DEPT_CD 
+AND A.ADDR LIKE '%수원%'
+AND A.EMP_NO  = C.EMP_NO 
+ORDER BY A.EMP_NO
+;
+
+SELECT 
+	A.EMP_NO 
+	, A.EMP_NM 
+	, A.ADDR 
+	, B.DEPT_CD 
+	, B.DEPT_NM 
+	, C.CERTI_CD 
+FROM TB_EMP A JOIN TB_DEPT B
+ON (A.DEPT_CD = B.DEPT_CD)
+JOIN TB_EMP_CERTI C 
+ON(A.EMP_NO = C.EMP_NO)
+WHERE A.ADDR LIKE '%수원%'
+;
+
